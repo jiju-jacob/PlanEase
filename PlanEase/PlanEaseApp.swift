aecc10952eb2035b15e4ef0d7a74ff65d5c01375
Jiju S Jacob <20jiju@gmail.com>
2023-09-29 12:32:26 +0530

    feat: Create new project

---
+++ PlanEase/PlanEaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanEaseApp.swift
+//  PlanEase
+//
+//  Created by JIju S Jacob on 29/09/23.
+//
+
+import SwiftUI
+
+@main
+struct PlanEaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
